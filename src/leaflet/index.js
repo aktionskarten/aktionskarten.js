a8fb8047a28b2415a8184298b7b96c6b7803b511
iom <iom@riseup.net>
2018-10-25 12:44:17 +0200

    Fix marker icon path
    Webpack resolves wrong paths because they are calculated dynamically.
    Set them static so we have the right one in our bundle in the end.

--- src/leaflet/index.js
+++ src/leaflet/index.js
@@ -9,6 +9,14 @@ import 'leaflet-styleeditor/dist/css/Leaflet.StyleEditor.min.css'
 import 'AktionskartenMarker/AktionskartenMarker.css'
 import './style.css'
 
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
+
 
 //
 // Localized Leaflet.Draw strings
