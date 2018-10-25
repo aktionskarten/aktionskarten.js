8dd129b818f069183025ad7718e658f13f8b8b76
iom <iom@riseup.net>
2018-10-25 13:33:06 +0200

    webpack - add leaflet to bundle

--- webpack.config.js
+++ webpack.config.js
@@ -19,9 +19,6 @@ module.exports = {
   devServer: {
     contentBase: './dist'
   },
-  externals: {
-    'leaflet': 'L',
-  },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new MiniCssExtractPlugin({
