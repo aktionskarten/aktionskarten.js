7f0842dc6afc279bd58f246733ee36acc8a2a0f2
iom <iom@riseup.net>
2020-10-31 19:15:31 +0100

    tests - try localhost instead of ipv4 only

--- tests/utils.js
+++ tests/utils.js
@@ -3,7 +3,7 @@ import {Api} from '../src/api'
 import puppeteer from 'puppeteer';
 
 
-const url = 'http://127.0.0.1:5000'
+const url = 'http://localhost:5000'
 const api = new Api(url)
 
 //api.errorHandler = (err) => {
