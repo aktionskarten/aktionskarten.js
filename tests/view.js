81ace37a6268f7180c228d9eb49879d5eea86577
iom <iom@riseup.net>
2020-10-31 17:50:56 +0100

    tests - add for different ratios

--- tests/view.js
+++ tests/view.js
@@ -2,16 +2,41 @@ import test from 'ava';
 import puppeteer from 'puppeteer';
 import {api, reset_db} from './utils'
 import {MapModel} from '../src/model'
+import haversine from 's-haversine';
+
+// Use to track xy coords in new tests. Add the folloing in your test case
+// await registerMouseEvents(page);
+function registerMouseEvents(page) {
+  page.evaluateOnNewDocument(type=>{
+    let callback = (e)=>{
+      console.info(e.type, e.clientX, e.clientY);
+      return true;
+    };
+    document.addEventListener('click', e=>callback(e))
+    document.addEventListener('mousedown', e=>callback(e))
+    document.addEventListener('mouseup', e=>callback(e))
+  });
+}
+
+function getSizes(bbox) {
+  //latitude: 52, longitude: 13
+  var southEast = bbox.slice(0,2).reverse(),
+      northWest = bbox.slice(2).reverse(),
+      northEast = [northWest[0], southEast[1]],
+      southWest = [southEast[0], northWest[1]]
+
+  let height = haversine.distance(southEast, northEast),
+      width = haversine.distance(southEast, southWest);
+
+  return [height, width];
+}
+
 
 test.beforeEach(async t => {
   await reset_db();
 });
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-test('page - draw bbox', async t => {
+test('page - draw bbox (landscape)', async t => {
   var model = new MapModel(api, {name: 'foo'});
   await model.save()
 
@@ -19,41 +44,133 @@ test('page - draw bbox', async t => {
 //    headless: false
   });
 	const page = await browser.newPage();
-  await page.setViewport({ width: 1440, height: 900 });
+  await page.setViewport({ width: 1024, height: 544 });
 
   const id = model.id
   const secret = model.secret
   var url = 'http://localhost:8080/#'+id+'/'+secret;
   await page.goto(url)
 
-  await sleep(1000)
-
-  // start drawing bbox
-  await page.mouse.click(720,128)
-  //await page.click('button.btn-primary')
-
-  await sleep(1000)
+  await page.waitForTimeout(1000)
 
   // draw bbox
-  await page.mouse.move(755,500)
+  await page.mouse.move(530,280)
   await page.mouse.down()
-  await page.mouse.move(775,500, {steps: 20})
+  await page.mouse.move(550,290, {steps: 20})
   await page.mouse.up()
 
-  await sleep(1000)
+  await page.waitForTimeout(1000)
 
   // commit bbox
-  await page.mouse.click(750,128)
-  //await page.click('button.btn-primary')
+  await page.mouse.click(600, 100);
 
-  await sleep(1000)
+  await page.waitForTimeout(1000)
 
   model= await MapModel.get(api, id, secret);
   t.is(model.bbox.length, 4)
 
+  let [height, width] = getSizes(model.bbox),
+      ratio = height/width,
+      landscape = 1240/1754.;
+
+  t.true(Math.abs(ratio-landscape) < 0.1)
+
   await page.close();
   await browser.close();
 });
+
+test('page - draw bbox (portrait)', async t => {
+  var model = new MapModel(api, {name: 'foo'});
+  await model.save()
+
+	const browser = await puppeteer.launch({
+//    headless: false
+  });
+	const page = await browser.newPage();
+  await page.setViewport({ width: 1024, height: 544 });
+
+  const id = model.id
+  const secret = model.secret
+  var url = 'http://localhost:8080/#'+id+'/'+secret;
+  await page.goto(url)
+
+  await page.waitForTimeout(1000)
+
+  await page.select('.container select', 'portrait')
+
+  await page.waitForTimeout(1000)
+
+  // draw bbox
+  await page.mouse.move(530,280)
+  await page.mouse.down()
+  await page.mouse.move(550,290, {steps: 20})
+  await page.mouse.up()
+
+  await page.waitForTimeout(1000)
+
+  // commit bbox
+  await page.mouse.click(600, 100);
+
+  await page.waitForTimeout(1000)
+
+  model= await MapModel.get(api, id, secret);
+  t.is(model.bbox.length, 4)
+
+  let [height, width] = getSizes(model.bbox),
+      ratio = height/width,
+      portrait = 1754/1240.;
+
+  t.true(Math.abs(ratio-portrait) < 0.1)
+
+  await page.close();
+  await browser.close();
+});
+
+
+test('page - draw bbox (no restrictions)', async t => {
+  var model = new MapModel(api, {name: 'foo'});
+  await model.save()
+
+	const browser = await puppeteer.launch({
+//    headless: false
+  });
+	const page = await browser.newPage();
+  await page.setViewport({ width: 1024, height: 544 });
+
+  const id = model.id
+  const secret = model.secret
+  var url = 'http://localhost:8080/#'+id+'/'+secret;
+  await page.goto(url)
+
+  await page.waitForTimeout(1000)
+
+  await page.select('.container select', '')
+
+  await page.waitForTimeout(1000)
+
+  // draw bbox
+  await page.mouse.move(530,280)
+  await page.mouse.down()
+  await page.mouse.move(550,300, {steps: 20})
+  await page.mouse.up()
+
+  await page.waitForTimeout(1000)
+
+  // commit bbox
+  await page.mouse.click(600, 100);
+
+  await page.waitForTimeout(1000)
+
+  model= await MapModel.get(api, id, secret);
+  t.is(model.bbox.length, 4)
+
+  let [height, width] = getSizes(model.bbox)
+  t.true(Math.abs(width-height) < 1)
+
+  await page.close();
+  await browser.close();
+});
+
 
 test('page - draw marker', async t => {
   const bbox = [ 13.412933, 52.496578, 13.424606, 52.501601 ]
@@ -64,7 +181,7 @@ test('page - draw marker', async t => {
 //    headless: false
   });
 	const page = await browser.newPage();
-  await page.setViewport({ width: 1440, height: 900 });
+  await page.setViewport({ width: 1024, height: 544 });
 
   const id = model.id
   const secret = model.secret
@@ -72,13 +189,10 @@ test('page - draw marker', async t => {
   await page.goto(url)
 
   try {
-    await page.waitForSelector('.leaflet-styleeditor-tooltip')
+    await page.waitForSelector('.leaflet-toolbar-editable-marker')
   } catch (error) {
-    console.log("Bounding box tooltip did not appear.")
+    console.log("Controls not rendered.")
   }
-
-  // confirm bbox
-  await page.click('button.btn-primary')
 
   // place markers
   for (var i=0; i<10;++i) {
@@ -90,7 +204,7 @@ test('page - draw marker', async t => {
       console.log("Marker could not be drawn.")
     }
 
-    await page.mouse.click(600+i*30,500)
+    await page.mouse.click(300+i*30,300)
 
     // check if style editor is open
     try {
