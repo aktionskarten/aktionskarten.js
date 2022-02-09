491270ba5f7b9086ef55842f084026d35f56fed9
kohllisti <kohllisti@riseup.net>
2022-02-09 15:00:15 +0100

    Wget should be quiet

--- docker-test-entrypoint.sh
+++ docker-test-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-while ! wget -t 1 ${BACKEND_HOST}:${BACKEND_PORT}/api/maps; do
+while ! wget -q -t 1 ${BACKEND_HOST}:${BACKEND_PORT}/api/maps; do
     echo "Connecting to ${BACKEND_HOST} Failed"
     sleep 1
 done
